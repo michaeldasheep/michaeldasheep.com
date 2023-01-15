00e3a50b0e614bedd4123216ebf7cac499e5de20
Michael Zhao <michael@michaeldasheep.com>
2023-01-15 07:29:03 +0000

    added Updater

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ~/kubernetes-manifests
+echo 
+kubectl delete -f michaeldasheep.com
+echo 
+echo ----------------
+echo DELETE DONE
+echo ----------------
+echo 
+kubectl apply -f michaeldasheep.com
+echo ----------------
+echo CREATE DONE
+echo ----------------
+echo 
+echo ----------------
+echo UPDATE COMPLETE
+echo ----------------
+echo 
