002ce453bc746042c4155634e2f8bd3fd61f2d76
Michael Zhao <michael@michaeldasheep.com>
2023-03-15 03:16:50 +0000

    added sheepjpg

--- html/index.php
+++ html/index.php
@@ -7,13 +7,16 @@
             body {
                 font-family: Arial, Helvetica, sans-serif;
             }
+            .navbar {
+                background-color: lightgray;
+            }
         </style>
     </head>
     <body>
-        <?php include 'includes/nav.php' ?>
+        <?php include 'includes/nav.php'; ?>
         <div class="navbar">
-            <div style="display: inline-block">
-                <h3 style="font-family: Arial, Helvetica, sans-serif;">michaeldasheep.com</h3>
+            <div style="display: inline-block; position:absolute; text-align:left">
+                <div class="logo"><img src="static/sheep.jpg" width="400" height="300" alt="michaeldasheep.com Logo" /></div>
             </div>
         </div>
     </body>
